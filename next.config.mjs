5f7a4c4ea71352219ea1b2b288b392fdd270ecca
Nan-Navi-60 <77kangsan60@gmail.com>
2026-02-11 20:24:42 +0900

    add build Setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',  // 핵심: 정적 파일(HTML)로 내보내기 설정
+  images: {
+    unoptimized: true, // GitHub Pages에서는 Next.js의 이미지 최적화 기능 사용 불가 (필수 설정)
+  },
 };
 
 export default nextConfig;
